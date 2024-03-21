85ac7013fef87d6056de501bbfd82630e12e78a9
Camille Crapat <camille.capt@gmail.com>
2024-03-21 10:44:39 +0100

    Création des routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import './global.css';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import Locations from './pages/Locations';
+import Location from './pages/Location';
+import About from './pages/About';
+import Error from './pages/Error';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+   <React.StrictMode>
+      <Header />
+      <Router>
+         <Routes>
+            <Route path="/" element={<Locations />} />
+            <Route path="/location/:id" element={<Location />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<Error />} />
+         </Routes>
+      </Router>
+      <Footer />
+   </React.StrictMode>,
+);
