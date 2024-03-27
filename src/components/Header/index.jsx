24a1e0014cc6427f438b5933169609c917eb9b7f
Camille Crapat <camille.capt@gmail.com>
2024-03-27 15:17:13 +0100

    Création header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,3 +1,33 @@
+import { NavLink } from 'react-router-dom';
+
+import './index.scss';
+
+import Logo from '../../assets/images/logo.svg';
+
 export default function Header() {
-   return <div></div>;
+   return (
+      <header>
+         <img src={Logo} alt="logo" />
+         <nav>
+            <ul>
+               <li>
+                  <NavLink
+                     to="/"
+                     className={({ isActive }) => (isActive ? `active` : ``)}
+                  >
+                     Accueil
+                  </NavLink>
+               </li>
+               <li>
+                  <NavLink
+                     to="/about"
+                     className={({ isActive }) => (isActive ? 'active' : '')}
+                  >
+                     A Propos
+                  </NavLink>
+               </li>
+            </ul>
+         </nav>
+      </header>
+   );
 }
