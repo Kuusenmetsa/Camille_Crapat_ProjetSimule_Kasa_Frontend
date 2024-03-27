9ec3f5b63e9660844dd4db110967a7688a529ac3
Camille Crapat <camille.capt@gmail.com>
2024-03-27 15:21:14 +0100

    Création de la page d'accueil

--- src/pages/Locations/index.jsx
+++ src/pages/Locations/index.jsx
@@ -1,3 +1,24 @@
+import './index.scss';
+
+import Card from '../../components/Card';
+import Cover from '../../components/Cover';
+
+import logements from '../../data/logements.json';
+
 export default function Locations() {
-   return <div></div>;
+   return (
+      <section>
+         <Cover type="home" text="Chez vous, partout et ailleurs" />
+         <div className="cards">
+            {logements.map((logement) => (
+               <Card
+                  id={logement.id}
+                  title={logement.title}
+                  cover={logement.cover}
+                  key={logement.id}
+               />
+            ))}
+         </div>
+      </section>
+   );
 }
