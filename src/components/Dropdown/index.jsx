9ffde2e664db1189476e73f3410687cda6076dce
Camille Crapat <camille.capt@gmail.com>
2024-04-04 10:08:05 +0200

    Création de la page à propos

---
+++ src/components/Dropdown/index.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+
+import './index.scss';
+
+import ChevronUp from '../../assets/icônes/chevron-up-solid.svg';
+import ChevronDown from '../../assets/icônes/chevron-down-solid.svg';
+
+export default function Dropdown({ title, content }) {
+   const [open, isOpen] = useState(false);
+   return (
+      <div className="dropdown" onClick={() => isOpen(!open)}>
+         <div className="dropdown__header">
+            <div className="dropdown__header__title">{title}</div>
+            <div className="dropdown__header__button">
+               {open ? (
+                  <img src={ChevronDown} alt="Boutton dropdown" />
+               ) : (
+                  <img src={ChevronUp} alt="Boutton dropdown" />
+               )}
+            </div>
+         </div>
+         <AnimatePresence>
+            {open && (
+               <motion.div
+                  className="dropdown__content"
+                  initial={{ height: 0, opacity: 0 }}
+                  animate={{ height: 'auto', opacity: 1 }}
+                  exit={{ height: 0, opacity: 0 }}
+                  transition={{ duration: 0.15 }}
+               >
+                  {title === 'Equipements' ? (
+                     <ul>
+                        {content.map((el) => (
+                           <li key={Date.now()}>{el}</li>
+                        ))}
+                     </ul>
+                  ) : (
+                     <p>{content}</p>
+                  )}
+               </motion.div>
+            )}
+         </AnimatePresence>
+      </div>
+   );
+}
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,3 +1,30 @@
+import './index.scss';
+
+import Cover from '../../components/Cover';
+import Dropdown from '../../components/Dropdown';
+
 export default function About() {
-   return <div></div>;
+   return (
+      <section>
+         <Cover type="about" text="" />
+         <div className="dropdowns">
+            <Dropdown
+               title="Fiabilité"
+               content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes"
+            />
+            <Dropdown
+               title="Respect"
+               content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+            />
+            <Dropdown
+               title="Service"
+               content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+            />
+            <Dropdown
+               title="Sécurité"
+               content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+            />
+         </div>
+      </section>
+   );
 }
