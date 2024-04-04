40a17b98c576ee746ab12cf2d106377e1983fbda
Camille Crapat <camille.capt@gmail.com>
2024-04-04 14:04:35 +0200

    Création de page d'erreur

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,3 +1,15 @@
+import { Link } from 'react-router-dom';
+
+import './index.scss';
+
+import ErrorSVG from '../../assets/images/404.svg';
+
 export default function Error() {
-   return <div></div>;
+   return (
+      <div className="error">
+         <img src={ErrorSVG} alt="Erreur 404" />
+         <p>Oups! La page que vous demandez n&apos;existe pas.</p>
+         <Link to="/">Retourner sur la page d’accueil</Link>
+      </div>
+   );
 }
